96bcd3fef55588d83b4280e90f04f872e09498e4
Xuqing Lin <xlin0057@student.monash.edu>
2026-06-12 16:03:35 +1000

    Paint Supply and Order Management System - Part One

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PaintManagementSystem.Enums;
+using PaintManagementSystem.Models;
+
+namespace PaintManagementSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int rate = 10;
+        bool isOverridable = true;
+
+        PaintProduct paint1 = new PaintProduct(
+            "Dulux Primer",
+            120.00m,
+            PaintType.BaseCoat,
+            new PaintSpecification("White", 10),
+            0.10m
+        );
+        PaintProduct paint2 = new PaintProduct(
+            "Glossy Wall Paint",
+            80.00m,
+            PaintType.Glossy,
+            new PaintSpecification("Blue", 5),
+            0.10m
+        );
+        PaintProduct paint3 = new PaintProduct(
+            "Matte Interior Paint",
+            150.00m,
+            PaintType.Matte,
+            new PaintSpecification("Grey", 15),
+            0.10m
+        ); 
+        PaintProduct[] products = { paint1, paint2, paint3 };
+
+        Console.WriteLine("Available Paint Products:");
+        Console.WriteLine();
+        foreach (PaintProduct product in products)
+        {
+            product.DisplayInfo(rate, isOverridable);
+        }
+
+        Order order = new Order(paint2, 3, rate, isOverridable);
+        Console.WriteLine();
+        order.DisplayOrder();
+    }
+}
